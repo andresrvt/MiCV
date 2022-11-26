955477daa29ef64790489bfff7a3bf95f630bfdc
Andresrvt <andresrvt@gmail.com>
2022-11-26 19:47:00 +0100

    commit skills y vida personal

--- js/custom.js
+++ js/custom.js
@@ -9,3 +9,32 @@ function esconder(id) {
     let pitumba  = document.getElementById(id);
     pitumba.classList.replace("d-block","d-none");
 }
+
+let circularProgress = document.querySelector(".circular-progress"),
+    progressValue = document.querySelector(".progress-value");
+
+let progressStartValue = 0,
+    progressEndValue = 100,
+    speed = 100;
+
+let progress = setInterval(()=>{
+    progressStartValue++;
+
+    progressValue.textContent = `${progressStartValue}%`
+    circularProgress.style = `background: conic-gradient(rgba(250, 2, 2, 0.973) ${progressStartValue*3.6}deg, white 0deg);`;
+
+    if (progressStartValue == progressEndValue) {
+        clearInterval(progress);
+    }
+}, speed);
+
+$( document ).ready(function() {
+    $(".setsize").each(function() {
+        $(this).height($(this).width());
+    });
+});
+$(window).on('resize', function(){
+    $(".setsize").each(function() {
+        $(this).height($(this).width());
+    });
+});
